8b37b8b6534cb5adbaec563156951b54a18dbdec
al357747 <al357747@gmail.com>
2025-10-21 12:02:45 -0700

    test merge with main

--- hello.py
+++ hello.py
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 print('Hello world')
+print('emergency fix 2')
 print('added to test git branch.')
 print('added only to file in the new branch')
-=======
-﻿print('Hello world')
-print('Hello world2: 10 21 2025')
-
->>>>>>> 5b3ca0f0694d8bab1fce1952f4a2a30e19981c8e
+print('emergency fix')
+print('new line to merge with the file in main')
